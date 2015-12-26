1118a9c0a25c750ed8fdfcfbe1a57f3e0029fa3b
Alisson R. Perez <alissonperez@gmail.com>
2015-12-26 02:53:37 -0200

    Add git-gutter to use-package

--- init.el
+++ init.el
@@ -97,29 +97,23 @@
 ;; Git gutter
 ;;============================================================
 
-(require 'git-gutter)
-
-;; If you enable global minor mode
-(global-git-gutter-mode t)
-
-;; If you would like to use git-gutter.el and linum-mode
-(git-gutter:linum-setup)
-
-;; If you enable git-gutter-mode for some modes
-(add-hook 'ruby-mode-hook 'git-gutter-mode)
-
-(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
-(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
-
-;; Jump to next/previous hunk
-(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
-(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
-
-;; Stage current hunk
-(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
-
-;; Revert current hunk
-(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+(use-package git-gutter
+  :ensure t
+  :bind (("C-c v =" . git-gutter:popup-hunk) ;; show hunk diff
+         ("C-c v p" . git-gutter:previous-hunk)
+         ("C-c v n" . git-gutter:next-hunk)
+         ("C-c v s" . git-gutter:stage-hunk)
+         ("C-c v r" . git-gutter:revert-hunk))
+  :diminish git-gutter-mode
+  :init
+  (global-git-gutter-mode t)
+  :config
+  (custom-set-variables
+   '(git-gutter:window-width 1)
+   '(git-gutter:modified-sign "●") ;; two space
+   '(git-gutter:added-sign "●")    ;; multiple character is OK
+   '(git-gutter:deleted-sign "●"))
+  )
 
 ;;============================================================
 ;; Appearance
