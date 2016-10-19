8f0a3131c89cd630ee41a1f72485d10d2d8236d9
Alisson Perez <alissonperez@gmail.com>
2016-10-19 00:37:32 -0200

    Add a better duplicate line feature

--- init.el
+++ init.el
@@ -390,13 +390,30 @@
 (add-hook 'buffer-list-update-hook
 	  'sl/display-header)
 
+;;============================================================
+;; Duplicate Line
+;;============================================================
+
+(defun duplicate-current-line (&optional n)
+  "duplicate current line, make more than 1 copy given a numeric argument"
+  (interactive "p")
+  (save-excursion
+    (let ((nb (or n 1))
+    	  (current-line (thing-at-point 'line)))
+      ;; when on last line, insert a newline first
+      (when (or (= 1 (forward-line 1)) (eq (point) (point-max)))
+    	(insert "\n"))
+
+      ;; now insert as many time as requested
+      (while (> n 0)
+    	(insert current-line)
+    	(decf n)))))
+
+(global-set-key (kbd "C-S-d") 'duplicate-current-line)
 
 ;;===========================================================
 ;; General shortcuts
 ;;============================================================
-
-;; Duplicate line with C-c C-d
-(global-set-key "\C-c\C-c" "\C-a\C- \C-n\M-w\C-y\C-p\C-e")
 
 ;; "Save as" a buffer
 (global-set-key (kbd "C-x C-a") 'rename-file-and-buffer)
