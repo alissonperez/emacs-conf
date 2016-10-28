51a50f4340751f19522389ea49ec6f1ad4982f82
Alisson R. Perez <alissonperez@gmail.com>
2016-10-28 16:02:04 -0200

    Move editor config to init.el end

--- init.el
+++ init.el
@@ -514,3 +514,13 @@
   (add-hook 'ruby-mode-hook 'robe-mode)
   (add-hook 'robe-mode-hook 'ac-robe-setup))
 
+;; ==========================================================
+;; Editor config
+;; ==========================================================
+
+(use-package editorconfig
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook (editorconfig-mode 1))
+  (add-hook 'text-mode-hook (editorconfig-mode 1))
+  )
