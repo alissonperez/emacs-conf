72f3c41d53c64ff7be464c2dd6310e6d68cc6b83
Alisson R. Perez <756802+alissonperez@users.noreply.github.com>
2021-09-16 13:46:36 -0300

    fix: package init

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(require 'package)
-
 ;; Load path to load third party libs
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packs"))
 
@@ -14,14 +12,14 @@ There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  ;; (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 
-(add-to-list 'package-archives
-             '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+(require 'package)
 
 ;; Better scroll
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
